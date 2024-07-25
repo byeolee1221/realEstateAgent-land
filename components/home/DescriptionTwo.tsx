d80d67acd7b1660ceffcb8530a32bdf0e8a99f29
byeolee1221 <zicsso1221@gmail.com>
2024-07-26 00:31:46 +0900

    style: 메인페이지 중개메모 더 알아보기탭 추가

--- components/home/DescriptionTwo.tsx
+++ components/home/DescriptionTwo.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import MemoDescription from "./MemoDescription";
 
 const DescriptionTwo = () => {
   const descriptionArr = [
     "자유양식으로 메모업로드 가능",
-    "부가적인 메모기능으로 메모의 확장 가능",
     "메모에 지도정보 첨부 가능",
   ];
 
@@ -12,14 +12,14 @@ const DescriptionTwo = () => {
     <div className="flex flex-col space-y-5 rounded-xl shadow-sm bg-green-100 pl-4 pr-10 pb-5">
       <div className="flex items-center space-x-2 pt-5">
         <Image src="/memo2.png" alt="메모" width={30} height={30} />
-        <h2 className="text-xl font-semibold">업무메모</h2>
+        <h2 className="text-xl font-semibold">중개메모</h2>
       </div>
       <h2 className="text-lg">
         중개업무 중 메모가 필요할 때 유용하게 사용해보세요!
       </h2>
       <p>
         중개업무 도중 메모를 하고, 나중에 확인할 때 메모를 못 찾는 경우에
-        대비해서 업무메모 서비스를 통해 안정된 중개업무를 누려보세요.
+        대비해서 중개메모 서비스를 통해 안정된 중개업무를 누려보세요.
       </p>
       <div className="flex flex-col space-y-1">
         {descriptionArr.map((item, i) => (
@@ -49,9 +49,7 @@ const DescriptionTwo = () => {
         >
           무료 사용해보기
         </Link>
-        <button className="border border-green-500 rounded-md text-green-500 hover:bg-green-500 hover:text-white  px-3 py-2 text-center transition-colors">
-          더 알아보기
-        </button>
+        <MemoDescription />
       </div>
       <img
         src={"/memoPage.png"}
---
+++ components/home/MemoDescription.tsx
@@ -0,0 +1,71 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+const MemoDescription = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="border border-green-500 rounded-md text-green-500 hover:bg-green-500 hover:text-white px-3 py-2 text-center transition-colors">
+          더 알아보기
+        </button>
+      </DialogTrigger>
+      <DialogContent className="w-[90%] rounded-lg h-3/5 overflow-auto">
+        <DialogHeader className="text-start border-b pb-2">
+          <DialogTitle>중개메모 설명</DialogTitle>
+          <DialogDescription>중개메모에서 제공하는 기능은 다음과 같습니다.</DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col space-y-6">
+          <div className="flex flex-col space-y-2">
+            <h2 className="font-bold text-lg text-green-500">자유양식으로 메모업로드 가능</h2>
+            <ul className="list-disc text-sm pl-4 flex flex-col space-y-2">
+              <li>
+                중개사님이 바쁜 업무환경에서 형식에 구애받지 않고 편하게 작성하실 수 있도록
+                자유양식으로 구성했습니다.
+              </li>
+              <li>
+                상담노트와 마찬가지로 작성하신 내용은 중개사님 외에는 확인할 수 없으니 안심하시고
+                사용해주세요.
+              </li>
+              <li>
+                추가적인 메모가 필요하시다면 기존 메모를 수정해서 사용하시거나 새로운 메모를
+                추가하며 사용하실 수 있습니다.
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <h2 className="font-bold text-lg text-green-500">메모에 지도정보 첨부 가능</h2>
+            <ul className="list-disc text-sm pl-4 flex flex-col space-y-2">
+              <li>
+                임장하시면서 눈여겨보았던 매물의 위치를 지도에 표시해보세요. 나중에 확인하실 때 해당
+                매물의 정보를 빠르게 상기할 수 있습니다.
+              </li>
+              <li>
+                여러 주소나 장소를 메모해 놓고, 언제든 지도의 위치를 바꿔서 저장하실수도 있습니다.
+              </li>
+            </ul>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <button
+                type="button"
+                className="bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded-md transition-colors"
+              >
+                닫기
+              </button>
+            </DialogClose>
+          </DialogFooter>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MemoDescription;
