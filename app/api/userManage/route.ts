9dbb727e08b6a06be9ef1949eac0f0a54c84e817
byeolee1221 <zicsso1221@gmail.com>
2024-08-14 21:24:03 +0900

    fix: userManage api에서 없는 자료에 대해 날짜 구하는 로직 삭제

--- app/api/userManage/route.ts
+++ app/api/userManage/route.ts
@@ -35,16 +35,7 @@ export async function GET(req: Request) {
     userQuerySnapshot.forEach((doc) => {
       let userData = doc.data();
       userData.id = doc.id;
-
-      const createdAt = new Date(userData.createdAt);
-      const formatDate = (date: Date) => {
-        const year = date.getFullYear();
-        const month = (date.getMonth() + 1).toString().padStart(2, "0");
-        const day = date.getDate().toString().padStart(2, "0");
-        return `${year}-${month}-${day}`;
-      }
-
-      userData.createdAt = formatDate(createdAt);
+      
       matchData.push(userData);
     });
 
