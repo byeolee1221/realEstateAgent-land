e3e8221e898b5bf30b3aa3d830d2c74d18b1cf89
byeolee1221 <zicsso1221@gmail.com>
2024-10-01 00:06:59 +0900

    style: setting 페이지 데스크탑 스타일 추가

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -29,27 +29,30 @@ const Setting = () => {
 
   return (
     <NavBar>
-      <div className="flex flex-col space-y-6 px-4 pt-20 pb-10">
-        <div className="bg-slate-100 px-4 py-2 flex items-center space-x-2 rounded-md shadow-sm">
-          {session ? (
-            <img src={session.user?.image!} alt="프로필" className="w-10 h-10 rounded-sm" />
-          ) : (
-            <Image
-              src="/user.png"
-              alt="프로필"
-              width={40}
-              height={40}
-              className="bg-slate-300 rounded-full p-1"
-            />
-          )}
-          {session ? (
-            <div className="flex flex-col text-sm">
-              <h1 className="font-bold">{session.user?.name}</h1>
-              <p>{session.user?.email}</p>
-            </div>
-          ) : (
-            <h2>로그인이 필요한 서비스입니다.</h2>
-          )}
+      <div className="flex flex-col space-y-6 px-4 pt-20 pb-10 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-8">
+        <div className="bg-slate-100 px-4 py-2 flex items-center space-x-2 rounded-md shadow-sm lg:flex-col lg:space-x-0 lg:space-y-2 lg:items-start lg:py-4">
+          <h2 className="hidden text-sm font-semibold lg:block">사용자 정보</h2>
+          <div className="flex items-center space-x-2">
+            {session ? (
+              <img src={session.user?.image!} alt="프로필" className="w-10 h-10 rounded-sm" />
+            ) : (
+              <Image
+                src="/user.png"
+                alt="프로필"
+                width={40}
+                height={40}
+                className="bg-slate-300 rounded-full p-1"
+              />
+            )}
+            {session ? (
+              <div className="flex flex-col text-sm">
+                <h1 className="font-bold">{session.user?.name}</h1>
+                <p>{session.user?.email}</p>
+              </div>
+            ) : (
+              <h2>로그인이 필요한 서비스입니다.</h2>
+            )}
+          </div>
         </div>
         <div className="bg-slate-100 flex flex-col rounded-md px-4 py-5 space-y-3 text-sm shadow-sm">
           <h2 className="font-semibold text-sm">사이트 메뉴</h2>
