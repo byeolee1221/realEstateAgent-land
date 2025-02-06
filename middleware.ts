f6a8d261e8f541d0b7e878f300f5905b74f93791
byeolee1221 <zicsso1221@gmail.com>
2025-02-06 22:29:40 +0900

    Chore: 미들웨어 코드 수정

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
  
-const allowedOrigins = ['http://localhost:3000/', 'https://real-estate-agent-land.vercel.app/']
+const allowedOrigins = ['https://real-estate-agent-land.vercel.app/']
  
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
