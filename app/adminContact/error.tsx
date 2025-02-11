141a1c9337989b1e2b43db7a6d3dc51b0c457847
byeolee1221 <zicsso1221@gmail.com>
2025-02-11 22:52:41 +0900

    Chore: error.tsx 이미지 경로 및 스타일 수정

--- app/adminContact/error.tsx
+++ app/adminContact/error.tsx
@@ -13,7 +13,7 @@ const AdminContactError = ({
     <div className="fixed inset-0 bg-white">
       <div className="mt-96 flex w-full flex-col items-center justify-center space-y-5">
         <div className="flex flex-col items-center space-y-3 pc:flex-row pc:space-x-3">
-          <Image src="/image/error.png" alt="에러 아이콘" width={60} height={60} />
+          <Image src="/error.png" alt="에러 아이콘" width={60} height={60} />
           <div className="flex flex-col space-y-1 text-center pc:text-start">
             <h1 className="text-xl font-bold tracking-tighter pc:text-2xl">
               {error.message}
